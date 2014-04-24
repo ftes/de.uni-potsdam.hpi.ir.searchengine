c15f19d3fa12114be5dfe0836be21daa3826b4ea
Fredrik Teschke <f@ftes.de>
2014-04-25 00:14:45 +0200

    added mock testing impls

---
+++ src/test/java/de/hpi/krestel/mySearchEngine/MockIndexFileHandlerImpl.java
@@ -0,0 +1,45 @@
+package de.hpi.krestel.mySearchEngine;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MockIndexFileHandlerImpl implements IndexFileHandler {
+	private final List<Term> terms;
+	private int index = 0;
+	private String id;
+	
+	public MockIndexFileHandlerImpl(String id, Term... terms) {
+		this.terms = new ArrayList<>(Arrays.asList(terms));
+		this.id = id;
+	}
+
+	@Override
+	public void close() throws IOException {}
+
+	@Override
+	public String getFilename() {
+		return id;
+	}
+
+	@Override
+	public Term readTerm(long fileOffset) throws IOException {
+		return terms.get((int) fileOffset);
+	}
+
+	@Override
+	public Term readNextTerm() throws IOException {
+		return index < terms.size() ? terms.get(index++) : null;
+	}
+
+	@Override
+	public long storeTerm(Term term) throws IOException {
+		terms.add(term);
+		return terms.indexOf(term);
+	}
+
+	public List<Term> getTerms() {
+		return terms;
+	}
+}
---
+++ src/test/java/de/hpi/krestel/mySearchEngine/MockSeekListImpl.java
@@ -0,0 +1,23 @@
+package de.hpi.krestel.mySearchEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockSeekListImpl implements SeekList {
+	private final Map<String, Long> offsets = new HashMap<>();
+
+	@Override
+	public long getTermOffsetInIndex(String term) throws TermLengthException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public void storeTermOffset(String term, long offset) {
+		offsets.put(term, offset);
+	}
+	
+	public Map<String, Long> getOffsets() {
+		return offsets;
+	}
+
+}
