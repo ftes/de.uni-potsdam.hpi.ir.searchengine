a47545eb4a18839bdb8f05b618be13507ee45b1b
Fredrik Teschke <f@ftes.de>
2014-04-25 00:15:41 +0200

    made term comparator resilient to occurences with equal term strings

--- src/main/java/de/hpi/krestel/mySearchEngine/TermComparator.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/TermComparator.java
@@ -3,7 +3,8 @@ package de.hpi.krestel.mySearchEngine;
 import java.util.Comparator;
 
 /**
- * Compares terms based by comparing the string representations.
+ * Compares terms based by comparing the string representations. Will give a random ordering
+ * if both have the same term, unless they share object identity.
  * @author fredrik
  *
  */
@@ -12,6 +13,10 @@ public enum TermComparator implements Comparator<Term> {
 	
 	@Override
 	public int compare(Term o1, Term o2) {
-		return o1.getTerm().compareTo(o2.getTerm());
+		int result = o1.getTerm().compareTo(o2.getTerm());
+		if (result == 0) {
+			return o1 == o2 ? 0 : -1;
+		}
+		return result;
 	}
 }
