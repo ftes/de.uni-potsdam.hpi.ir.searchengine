cd33c9c30a5df89465a1de83d7d168d7aa570277
Alexander Teibrich <alexander@teibrich.de>
2014-04-24 16:50:10 +0200

    File Handling Specification

---
+++ src/main/java/de/hpi/krestel/mySearchEngine/TermOccurence.java
@@ -0,0 +1,46 @@
+package de.hpi.krestel.mySearchEngine;
+
+/**
+ * This represents a single occurence of a termin in a certain document.
+ * The termoccurence is referenced in the Term.
+ * 
+ * @author Alexander
+ *
+ */
+public class TermOccurence {
+	/**
+	 * The ID of the document as specified in the XML
+	 */
+	private int documentId;
+	/**
+	 * The absolute position of the term in the document
+	 */
+	private int position;
+	
+	public TermOccurence() {
+	}
+	public TermOccurence(int documentId, int position) {
+		super();
+		this.documentId = documentId;
+		this.position = position;
+	}
+	
+	public int getDocumentId() {
+		return documentId;
+	}
+	public void setDocumentId(int documentId) {
+		this.documentId = documentId;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+	@Override
+	public String toString() {
+		return "TermOccurence [documentId=" + documentId + ", position="
+				+ position + "]";
+	}	
+}
