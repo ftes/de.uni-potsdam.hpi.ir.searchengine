d342019b491b95fca1a9aadfe3e6b49b1a6cff46
PJung <xpl@gmx.de>
2014-04-25 13:53:40 +0200

    tokenizing adaptations

--- src/test/java/de/hpi/krestel/mySearchEngine/WikipediaExport.java
+++ src/test/java/de/hpi/krestel/mySearchEngine/WikipediaExport.java
@@ -1,6 +1,7 @@
 package de.hpi.krestel.mySearchEngine;
 
 import java.io.FileNotFoundException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,51 +24,105 @@ public class WikipediaExport {
 		String tag = null;
 
 		boolean inRevision = false;
+		
+		
+		Runtime runtime = Runtime.getRuntime();
 
-		while (parser.hasNext()) {
+	    NumberFormat format = NumberFormat.getInstance();
 
-			switch (parser.getEventType()) {
-			case XMLStreamConstants.START_DOCUMENT:
-				break;
+	    StringBuilder sb1 = new StringBuilder();
+	    long maxMemory = runtime.maxMemory();
+	    long allocatedMemory = runtime.totalMemory();
+	    long freeMemory = runtime.freeMemory();
 
-			case XMLStreamConstants.END_DOCUMENT:
-				parser.close();
-				break;
-
-			case XMLStreamConstants.START_ELEMENT:
-				tag = parser.getLocalName();
-				if (tag.equals("page")) {
-					page = new Page();
-				} else if (tag.equals("revision")) {
-					inRevision = true;
-				} else if (tag.equals("text")) {
-					characters = parser.getElementText(); // parses whole text and checks for end event
-					page.setText(characters);
-				}
-				break;
-
-			case XMLStreamConstants.CHARACTERS:
-				characters = parser.getText();
-				break;
-
-			case XMLStreamConstants.END_ELEMENT:
-				tag = parser.getLocalName();
-				if (! inRevision && tag.equals("id")) {
-					page.setId(Integer.parseInt(characters));
-				} else if (! inRevision && tag.equals("title")) {
-					page.setTitle(characters);
-				} else if (tag.equals("page")) {
-					pages.add(page);
-					System.out.println(page);
-				} else if (tag.equals("revision")) {
-					inRevision = false;
-				}
-				break;
-
-			default:
+	    sb1.append("free memory: " + format.format(freeMemory / 1024) + "<br/>");
+	    sb1.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
+	    sb1.append("max memory: " + format.format(maxMemory / 1024) + "<br/>");
+	    sb1.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
+	    
+	    System.out.println(sb1.toString());
+	    
+		boolean memoryExhausted = false;
+		int exhaustion = 10000;
+		
+		while (!memoryExhausted){
+			
+			// check again
+			if (exhaustion == 0){
+				memoryExhausted = true;
 				break;
 			}
-			parser.next();
+			
+			// read from stream
+			while (parser.hasNext()) {
+				
+				switch (parser.getEventType()) {
+				case XMLStreamConstants.START_DOCUMENT:
+					break;
+
+				case XMLStreamConstants.END_DOCUMENT:
+					parser.close();
+					break;
+
+				case XMLStreamConstants.START_ELEMENT:
+					tag = parser.getLocalName();
+					if (tag.equals("page")) {
+						page = new Page();
+					} else if (tag.equals("revision")) {
+						inRevision = true;
+					} else if (tag.equals("text")) {
+						characters = parser.getElementText(); // parses whole text and checks for end event
+						page.setText(characters);
+										
+						// filter text
+						String[] tokens = characters.split("[\\s.]");
+						
+						for(String token : tokens){
+							token = token.toLowerCase();
+							token = token.replaceAll("[^a-zäöüß]", "");
+							System.out.println(token);
+							
+							// TODO: use stemmer
+						}
+						
+
+						
+					}
+					break;
+
+				case XMLStreamConstants.CHARACTERS:
+					characters = parser.getText();
+					break;
+
+				case XMLStreamConstants.END_ELEMENT:
+					tag = parser.getLocalName();
+					if (! inRevision && tag.equals("id")) {
+						page.setId(Integer.parseInt(characters));
+					} else if (! inRevision && tag.equals("title")) {
+						page.setTitle(characters);
+					} else if (tag.equals("page")) {
+						pages.add(page);
+						//System.out.println(page);
+					} else if (tag.equals("revision")) {
+						inRevision = false;
+					}
+					break;
+
+				default:
+					break;
+				}
+				
+				// TODO: after complete page check for memory exhaustion
+				
+				parser.next();
+			}
+			
+			// clean content
+			
+			// tokenize
+			
+			exhaustion--;
 		}
+		
 	}
 }
