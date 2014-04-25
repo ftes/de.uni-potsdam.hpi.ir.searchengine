475e8b18a3e33025ad3ef4cd5d23b25e00f8f3c1
Fredrik Teschke <f@ftes.de>
2014-04-25 14:59:16 +0200

    seeklist test

---
+++ src/test/java/de/hpi/krestel/mySearchEngine/SeekListTest.java
@@ -0,0 +1,34 @@
+package de.hpi.krestel.mySearchEngine;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class SeekListTest {
+	@Test
+	public void testWritingAndReading() throws IOException, TermLengthException {
+		String fileName = "seekListTest.dat";
+		
+		SeekList seekList = new SeekListImpl(fileName);
+		seekList.storeTermOffset("a", 1);
+		seekList.storeTermOffset("b", 2);
+		seekList.storeTermOffset("c", 3);
+		seekList.storeTermOffset("delta", 4);
+		seekList.storeTermOffset("e", 5);
+		seekList.storeTermOffset("f", 6);
+		seekList.storeTermOffset("genauzwanzigzeichen.", 7);
+		seekList.storeTermOffset("h", 8);
+		seekList.storeTermOffset("i", 9);
+		
+		assertEquals(5, seekList.getTermOffsetInIndex("e")); //in middle
+		assertEquals(1, seekList.getTermOffsetInIndex("a")); //very first
+		assertEquals(8, seekList.getTermOffsetInIndex("h")); //very last
+		assertEquals(7, seekList.getTermOffsetInIndex("genauzwanzigzeichen.")); //max length
+		assertEquals(-1, seekList.getTermOffsetInIndex("z")); //not in seeklist
+		
+		new File(fileName).delete();
+	}
+}
