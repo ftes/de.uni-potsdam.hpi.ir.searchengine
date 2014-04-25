5e7f868f2b64a0afa3d751b3c11229e16f9db747
Alexander Teibrich <alexander@teibrich.de>
2014-04-25 14:58:52 +0200

    abstract class Parser -> to add dummy parser

--- src/main/java/de/hpi/krestel/mySearchEngine/Parser.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/Parser.java
@@ -1,17 +1,27 @@
 package de.hpi.krestel.mySearchEngine;
 
-
-public class Parser {
+/**
+ * This Parser parses a given wikipedia xml file.
+ * 
+ * @author Alexander
+ *
+ */
+public abstract class Parser {
 	private String filename;
+	
+	/**
+	 * Creates the parser
+	 * @param filename the filename of the wikipedia xml file
+	 */
 	public Parser(String filename) {
-		// TODO Auto-generated constructor stub
+		this.filename = filename;
 	}
+	
 	/**
 	 * Parses the specified XML File and request the generation of the indexes.
 	 */
-	public void parseToPartialIndexes(String indexDirectory) {
-		
-	}
+	public abstract void parseToPartialIndexes(String indexDirectory);
+	
 	public String getFilename() {
 		return filename;
 	}
