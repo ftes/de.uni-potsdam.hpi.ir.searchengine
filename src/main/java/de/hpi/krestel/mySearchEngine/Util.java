2ef1ce76652add0532140f76e88914c3abf84931
Alexander Teibrich <alexander@teibrich.de>
2014-06-13 13:03:11 +0200

    limit to 1.6 GB of RAM

--- src/main/java/de/hpi/krestel/mySearchEngine/Util.java
+++ src/main/java/de/hpi/krestel/mySearchEngine/Util.java
@@ -83,7 +83,8 @@ public class Util {
 	/**
 	 * Leave at least 50 MBytes of memory free.
 	 */
-	public static final long MIN_FREE_MEMORY_BYTES = 50 * 1024 * 1024;
+	public static final long MIN_FREE_MEMORY_BYTES = 100 * 1024 * 1024;
+	public static final long MAX_USED_MEMORY_BYTES = 1600 * 1024 * 1024;
 	
 	/**
 	 * Determine whether the main memory can be considered full.
@@ -92,7 +93,9 @@ public class Util {
 	 * TODO caching
 	 */
 	public static boolean isMainMemoryFull() {
-		return Runtime.getRuntime().freeMemory() < MIN_FREE_MEMORY_BYTES;
+		Runtime runtime = Runtime.getRuntime();
+		long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+		return usedMemory > MAX_USED_MEMORY_BYTES;
 	}
 	
 	/**
